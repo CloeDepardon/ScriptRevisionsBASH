a806199706f0fd9179ba992e861f7758547167e0
Cloé Depardon <cloedepardon@gmail.com>
2018-05-30 10:19:42 +0200

    modifs

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ function menu {
     RED='\033[0;31m'
     NC='\033[0m' # No Color
     options=("Accéder à un dossier" "Quitter")
-    optionsAcces=("Ouvrir un fichier" "Renommer un fichier" "Copier un fichier" "Supprimer un fichier" "Chercher un terme dans un fichier" "Créer un dossier" "Ouvrir un dossier" "Renommer un dossier" "Supprimer un dossier" "Retour au menu principal" "Quitter") 
+    optionsAcces=("Ouvrir un fichier" "Renommer un fichier" "Copier un fichier" "Supprimer un fichier" "Chercher un terme dans un fichier" "Créer un dossier" "Ouvrir un dossier" "Renommer un dossier" "Supprimer un dossier" "Afficher une image" "Retour au dossier précédent" "Retour au menu principal" "Quitter") 
     echo -e "${COLOR}Sélectionnez une action ${NC}"
     select responseAction in "${options[@]}";do
         case ${responseAction} in
@@ -29,9 +29,9 @@ function menu {
     COLOR='\033[1;34m'
     GREEN='\033[1;32m'
     NC='\033[0m' # No Color
-        echo -e "${COLOR}Vous êtes ici :${NC}"
+        echo -e "${GREEN}Vous êtes ici :${NC}"
         pwd
-        echo -e "${COLOR}Contenu du dossier :${NC}"
+        echo -e "${GREEN}Contenu du dossier :${NC}"
         ls
         echo -e "${COLOR}Que voulez-vous faire ? ${NC}"
         select responseAcces in "${optionsAcces[@]}";do
@@ -45,6 +45,8 @@ function menu {
                 "Ouvrir un dossier" ) choiceAcces="ouvrirDossier";break;;
                 "Renommer un dossier" ) choiceAcces="renommerDossier";break;;
                 "Supprimer un dossier" ) choiceAcces="supprimerDossier";break;;
+                "Afficher une image" ) choiceAcces="afficherImage";break;;
+                "Retour au dossier précédent" ) choiceAcces="retourDossier";break;;
                 "Retour au menu principal" ) choiceAcces="Retour";break;;
                 Quitter ) choiceAcces="Quitter";break;;
             esac
@@ -54,10 +56,11 @@ function menu {
         if [ "$choiceAcces" == "ouvrirFichier" ]
         then
             echo -e "${COLOR}Tous les fichiers disponibles : ${NC}"
-            ls -al | grep '^-'
+            ## EXCLURE IMAGES ##
+            find . -not -name "*.gif" -not -name "*.jpg" -not -name "*.png" -type f
             echo -e "${COLOR}Nom du fichier auquel vous souhaitez accéder : ${NC}"
             read -e nomFichier
-            echo -e "${COLOR}Contenu du fichier :${NC}"
+            echo -e "${GREEN}Contenu du fichier :${NC}"
             cat $nomFichier
             sousmenuAcces
         fi
@@ -66,7 +69,8 @@ function menu {
         if [ "$choiceAcces" == "renommerFichier" ]
         then
             echo -e "${COLOR}Tous les fichiers disponibles :${NC}"
-            ls -al | grep '^-'
+            ## EXCLURE IMAGES ##
+            find . -not -name "*.gif" -not -name "*.jpg" -not -name "*.png" -type f
             echo -e "${COLOR}Nom actuel du fichier que vous souhaitez renommer :${NC}"
             read -e nomActuelFichier
             echo -e "${COLOR}Nouveau nom pour ce fichier :${NC}"
@@ -88,11 +92,11 @@ function menu {
         if [ "$choiceAcces" == "chercherDansFichier" ]
         then
             echo -e "${COLOR}Tous les fichiers disponibles : ${NC}"
-            ls -al | grep '^-'
+            ## EXCLURE IMAGES ##
+            find . -not -name "*.gif" -not -name "*.jpg" -not -name "*.png" -type f
             echo -e "${COLOR}Terme que vous souhaitez rechercher dans les fichiers : ${NC}"
             read -e research
             find . -type f -print | xargs grep "$research"
-            
             sousmenuAcces
         fi
         
@@ -121,7 +125,7 @@ function menu {
         if [ "$choiceAcces" == "copierFichier" ]
         then
             echo -e "${COLOR}Tous les fichiers disponibles :${NC}"
-            ls -al | -e grep '^-'
+            ls -al | grep '^-'
             echo -e "${COLOR}Nom actuel du fichier que vous souhaitez copier :${NC}"
             read -e nomFichier
             cp $nomFichier $nomFichier."copie"
@@ -153,6 +157,16 @@ function menu {
             cd $nomDossier/
             sousmenuAcces
         fi
+         #Ouvrir une image
+        if [ "$choiceAcces" == "afficherImage" ]
+        then
+            echo -e "${COLOR}Toutes les images disponibles :${NC}"
+            find . -type f -exec file {} \; | awk -F: '{if ($2 ~/image/) print $1}'
+            echo -e "${COLOR}Nom de l'image que vous souhaitez ouvrir :${NC}"
+            read -e nomImage
+            xdg-open $nomImage/
+            sousmenuAcces
+        fi
         #Supprimer un fichier
         if [ "$choiceAcces" == "supprimerDossier" ]
         then
@@ -172,7 +186,7 @@ function menu {
                 fi
             sousmenuAcces
         fi
-        #Renommer un fichier
+        #Renommer un dossier
         if [ "$choiceAcces" == "renommerDossier" ]
         then
             echo -e "${COLOR}Tous les dossiers disponibles :${NC}"
@@ -184,21 +198,43 @@ function menu {
             echo -e "${COLOR}Souhaitez vous renommer $nomActuelDossier en $nouveauNomDossier ? (O/n)${NC}"  
             read -e responseRename
                 if [ "$responseRename" == "O" ]
+                    then
+                        mv $nomActuelDossier $nouveauNomDossier
+                        echo -e "${GREEN}Le dossier a bien été renommé ${NC}"
+                        find [0-9a-zA-Z]* -type d
+                    else 
+                    sousmenuAcces
+                fi
+                sousmenuAcces
+        fi
+         #RetourDossier
+        if [ "$choiceAcces" == "retourDossier" ]
+        then
+         echo -e "${COLOR}Souhaitez vous retourner dans le dossier précédent ? (O/n) ${NC}"
+            read -e responseMove
+                if [ "$responseMove" == "O" ]
                 then
-                    mv $nomActuelDossier $nouveauNomDossier
-                    echo -e "${GREEN}Le dossier a bien été renommé ${NC}"
-                    find [0-9a-zA-Z]* -type d
+                    cd ~- ## UNE SEULE FOIS - A CORRIGER ##
                 else 
                   sousmenuAcces
                 fi
-            sousmenuAcces
         fi
+        sousmenuAcces
+        
         #Retour
         if [ "$choiceAcces" == "Retour" ]
         then
             reset
             menu
         fi
+        #Quitter
+        if [ "$choiceAcces" == "Quitter" ]
+        then
+            echo -e "${GREEN}A bientôt${NC}"
+            exit;
+        fi
+        sousmenuAcces
+        
         #Quitter
         if [ "$choiceAcces" == "Quitter" ]
         then
@@ -218,10 +254,6 @@ function menu {
         echo -e "${COLOR}Nom du dossier auquel vous souhaitez accéder :${NC}"
         read -e nomDossier
         cd $nomDossier
-        echo -e "${COLOR}Vous êtes ici :${NC}" 
-        pwd
-        echo -e "${GREEN}Voici les fichiers présents dans $nomDossier ${NC}"
-        ls
         sousmenuAcces
     fi
 }
